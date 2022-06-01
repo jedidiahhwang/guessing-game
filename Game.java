1bcb90467a1c5e08bc67dd4cbc8ee50e08bad7ed
Jedidiah <jedidiah.hwang.dev@gmail.com>
2022-05-31 20:16:01 -0600

    finished number guessing game

--- Game.java
+++ Game.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*
 
 A number-guessing game.
@@ -7,7 +9,27 @@ A number-guessing game.
 public class Game {
   public static void main(String[] args){
     // Put your code here
-    System.out.println("Hello world!!!");
+    int num = 34;
+    int counter = 1;
+
+    System.out.println("Greetings, what is your name?");
+    Scanner scanner = new Scanner(System.in);
+    String userInputName = scanner.nextLine();
+
+    System.out.println(String.format("%s, pick a number between 1 and 100.", userInputName));
+    int numGuess = scanner.nextInt();
+    while(numGuess != num) {
+      counter++;
+      if(numGuess > num) {
+        System.out.println("Your number is too high.");
+      } else {
+        System.out.println("Your number is too low.");
+      }
+      System.out.println("Guess again.");
+      numGuess = scanner.nextInt();
+    }
+
+    System.out.println(String.format("%s, you found my number in %d", userInputName, counter));
   }
 
 }
